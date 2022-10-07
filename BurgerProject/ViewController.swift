0c4d4a091470174988103d4442b6aac26ea5f2f5
Braedon Larsen <braedon.larsen23@d155.org>
2022-10-07 07:52:48 -0500

    Initial Commit

---
+++ BurgerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerProject
+//
+//  Created by BRAEDON LARSEN on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
